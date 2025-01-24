a1eee8bca0b91c964e554ab9c9adf04efb4888b5
krishharyani <krishharyani@gmail.com>
2025-01-24 17:05:12 -0500

    added the maze class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -2,7 +2,10 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.CommandLine;
@@ -44,10 +47,66 @@ public class Main {
                 logger.error("Wrong format, please use '-i'");
             }
         } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\");
+            logger.error("An error has occured");
         }
         logger.info("**** Computing path");
         logger.info("PATH NOT COMPUTED");
         logger.info("** End of MazeRunner");
     }
 }
+
+
+class Maze {
+    private char[][] mazegrid;
+    private int rows = 0;
+    private int columns = 0;
+
+    public Maze(String inputfile) throws IOException {
+        initializeMaze(inputfile);
+    }
+
+    private void initializeMaze(String inputfile) throws IOException {
+        FileReader filereader = new FileReader(inputfile);
+        BufferedReader bufferedreader = new BufferedReader(filereader);
+        String currentLine;
+        while ((currentLine = bufferedreader.readLine()) != null) {
+            if (rows == 0) {
+                columns = currentLine.length();
+            }
+            rows++;
+        }
+        mazegrid = new char[rows][columns];
+
+        FileReader filereader2 = new FileReader(inputfile);
+        BufferedReader bufferedreader2 = new BufferedReader(filereader2);
+        int x = 0;
+        while ((currentLine = bufferedreader2.readLine()) != null) {
+            for (int y = 0; y < currentLine.length(); y++) {
+                mazegrid[x][y] = currentLine.charAt(y);
+            }
+            x++;
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public char[][] getMazegrid() {
+        return mazegrid;
+    }
+
+    public void displayMazegrid() {
+        for (int x = 0; x < getRows(); x++) {
+            for (int y = 0; y < getColumns(); y++) {
+                System.out.print(mazegrid[x][y]);
+            }
+            System.out.println();
+        }
+    }
+}
+
